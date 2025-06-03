51d4eb0d70cf2c07b480d4fb41eb498cb385d816
michaelmusembi <m.musembi@alustudent.com>
2025-06-03 02:45:46 -0700

    pycodestyle validation & correct output

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,26 +1,14 @@
 #!/usr/bin/env python3
-"""
-summation_i_squared.py
+"""Calculates the sum of squares of n natural numbers"""
 
-This module contains a recursive function to calculate the summation of
-squares from 1 to n, i.e., ∑(i^2) for i = 1 to n.
-"""
 
 def summation_i_squared(n):
     """
-    Calculates the sum of squares from 1 to n using recursion.
-
-    Args:
-        n (int): The upper bound of the summation (inclusive).
-
-    Returns:
-        int: The sum of squares from 1 to n.
-             Returns None if the input is not a valid positive integer.
+    Calculates the sum of the squares of the first n natural numbers
+    Utilizes Faulhaber's formula for power of 2:
+    sum of i^2 from i=1 to n = (n * (n + 1) * (2n + 1)) / 6
     """
-    if not isinstance(n, int) or n < 1:
+    if type(n) is not int or n < 1:
         return None
-
-    if n == 1:
-        return 1
-
-    return n * n + summation_i_squared(n - 1)
+    sigma_sum = (n * (n + 1) * ((2 * n) + 1)) / 6
+    return int(sigma_sum)
